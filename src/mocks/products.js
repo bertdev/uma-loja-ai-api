33c674972051c4617be91363fe7fab64cf0a51d1
herbert henrique <bert@pop-os.localdomain>
2021-08-21 20:06:25 -0300

    Adiciona mock de products

---
+++ src/mocks/products.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Iphone',
+    price: 3999,
+    urlImage: 'https://www.google.com/',
+  },
+  {
+    id: 2,
+    name: 'Ipad',
+    price: 4999,
+    urlImage: 'https://www.google.com/',
+  },
+  {
+    id: 3,
+    name: 'Macbook',
+    price: 13999,
+    urlImage: 'https://www.google.com/',
+  },
+];
