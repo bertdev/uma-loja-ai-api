a4134e3c1f6ec1b9e22998b3f35703903e485a94
herbert henrique <bert@pop-os.localdomain>
2021-08-21 20:52:09 -0300

    Adiciona o metodo show no controller de products

--- src/app/controllers/ProductsController.js
+++ src/app/controllers/ProductsController.js
@@ -4,6 +4,18 @@ class ProductsController {
   index(request, response) {
     response.json(products);
   }
+
+  show(request, response) {
+    const { id } = request.params;
+
+    const productExists = products.find((product) => product.id === Number(id));
+
+    if (!productExists) {
+      return response.status(404).json({ error: 'User not found' });
+    }
+
+    response.json(productExists);
+  }
 }
 
 module.exports = new ProductsController();
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const ProductsController = require('./app/controllers/ProductsController');
 const router = express();
 
 router.get('/products', ProductsController.index);
-// router.get('/products/:id', ProductsController.show);
+router.get('/products/:id', ProductsController.show);
 // router.post('/products', ProductsController.store);
 // router.put('/products/:id', ProductsController.update);
 // router.delete('/products/:id', ProductsController.delete);
